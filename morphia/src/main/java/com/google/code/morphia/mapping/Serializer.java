2fe2dae5fbec7ceea326a0d04b3be579cdaaa001
vassili <vassili@addepar.com>
2012-03-08 20:41:53 -0800

    1. Replaced GZIP with Snappy. Speed increase is tremendous, you gotta see it to believe it. 2. I am probably missing something but I have objects which are not Binary and not byte array and I want them raw instead of getting exception.

--- morphia/src/main/java/com/google/code/morphia/mapping/Serializer.java
+++ morphia/src/main/java/com/google/code/morphia/mapping/Serializer.java
@@ -1,7 +1,11 @@
 /**
- * 
+ *
  */
 package com.google.code.morphia.mapping;
+
+import org.bson.types.Binary;
+import org.xerial.snappy.SnappyInputStream;
+import org.xerial.snappy.SnappyOutputStream;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -10,62 +14,58 @@ import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
-import java.util.zip.GZIPInputStream;
-import java.util.zip.GZIPOutputStream;
-
-import org.bson.types.Binary;
 
 /**
  * @author Uwe Schaefer, (us@thomas-daily.de)
  */
 public class Serializer
 {
-	/** serializes object to byte[] */
-	public static byte[] serialize(final Object o, final boolean zip) throws IOException
-	{
-		final ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		OutputStream os = baos;
-		if (zip)
-		{
-			os = new GZIPOutputStream(os);
-		}
-		final ObjectOutputStream oos = new ObjectOutputStream(os);
-		oos.writeObject(o);
-		oos.flush();
-		oos.close();
-		
-		return baos.toByteArray();
-	}
-	
-	/** deserializes DBBinary/byte[] to object */
-	public static Object deserialize(final Object data, final boolean zipped) throws IOException,
-	ClassNotFoundException
-	{
-		ByteArrayInputStream bais;
-		if (data instanceof Binary)
-		{
-			bais = new ByteArrayInputStream(((Binary) data).getData());
-		}
-		else
-		{
-			bais = new ByteArrayInputStream((byte[]) data);
-		}
-		
-		InputStream is = bais;
-		try
-		{
-			if (zipped)
-			{
-				is = new GZIPInputStream(is);
-			}
-			
-			final ObjectInputStream ois = new ObjectInputStream(is);
-			return ois.readObject();
-		}
-		finally
-		{
-			is.close();
-		}
-	}
-	
+    /** serializes object to byte[] */
+    public static byte[] serialize(final Object o, final boolean zip) throws IOException
+    {
+        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        OutputStream os = baos;
+        if (zip)
+        {
+            os = new SnappyOutputStream(os);// Snappy! YAY! now we serialize/deserialize 20 times faster!
+        }
+        final ObjectOutputStream oos = new ObjectOutputStream(os);
+        oos.writeObject(o);
+        oos.flush();
+        oos.close();
+
+        return baos.toByteArray();
+    }
+
+    /** deserializes DBBinary/byte[] to object */
+    public static Object deserialize(final Object data, final boolean zipped) throws IOException,
+            ClassNotFoundException
+    {
+        ByteArrayInputStream bais;
+        if (data instanceof Binary)
+        {
+            bais = new ByteArrayInputStream(((Binary) data).getData());
+        }
+        else
+        {
+            bais = new ByteArrayInputStream((byte[]) data);
+        }
+
+        InputStream is = bais;
+        try
+        {
+            if (zipped)
+            {
+                is = new SnappyInputStream(is);// Snappy! YAY! now we serialize/deserialize 20 times faster!
+            }
+
+            final ObjectInputStream ois = new ObjectInputStream(is);
+            return ois.readObject();
+        }
+        finally
+        {
+            is.close();
+        }
+    }
+
 }
